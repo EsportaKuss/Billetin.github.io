eb3f2483bedbaa379b9b9b98563047d2eb00600d
EsportaKuss <124007387+EsportaKuss@users.noreply.github.com>
2023-05-10 16:43:26 -0600

    videos!

--- js/carrue.js
+++ js/carrue.js
@@ -8,3 +8,17 @@ $(function() {
     nav: true
   });
 });
+
+// You don't need JavaScript to display a video on your page, but you may use it to manipulate the video or add additional functionality.
+
+// For example, you can play or pause the video with JavaScript:
+
+const myVideo = document.getElementById("my-video");
+
+function playVideo() {
+  myVideo.play();
+}
+
+function pauseVideo() {
+  myVideo.pause();
+}
