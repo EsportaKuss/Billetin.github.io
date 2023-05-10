e77b3758c9737d9e67ab661ef7b30ad1bd66a9d5
EsportaKuss <124007387+EsportaKuss@users.noreply.github.com>
2023-05-10 15:57:16 -0600

    carrusel

--- js/carrue.js
+++ js/carrue.js
@@ -1,54 +1,10 @@
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
-const prevBtn = document.querySelector('.carousel-prev');
-const nextBtn = document.querySelector('.carousel-next');
-
-let counter = 1;
-const size = carouselImages[0].clientWidth;
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-nextBtn.addEventListener('click', () => {
-  if (counter >= carouselImages.length - 1) return;
-  carouselSlide.style.transition = 'transform 0.3s ease-in-out';
-  counter++;
-  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-prevBtn.addEventListener('click', () => {
-  if (counter <= 0) return;
-  carouselSlide.style.transition = 'transform 0.3s ease-in-out';
-  counter--;
-  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-carouselSlide.addEventListener('transitionend', () => {
-  if (carouselImages[counter].id === 'lastClone') {
-    carouselSlide.style.transition = 'none';
-    counter = carouselImages.length - 2;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-  }
-  if (carouselImages[counter].id === 'firstClone') {
-    carouselSlide.style.transition = 'none';
-    counter = carouselImages.length - counter;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-  }
-});
-
-// Modificación para mostrar todas las imágenes
-function adjustCarousel() {
-  const newImageCount = carouselImages.length;
-  const newCarouselWidth = size * newImageCount;
-
-  carouselSlide.style.width = newCarouselWidth + 'px';
-
-  Array.from(carouselImages).forEach((image, index) => {
-    image.style.width = size + 'px';
-    image.style.height = 'auto';
+$(function() {
+  // Owl Carousel
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: 3,
+    margin: 10,
+    loop: true,
+    nav: true
   });
-}
-
-// Espera a que se carguen todas las imágenes antes de ajustar el carrusel
-window.addEventListener('load', adjustCarousel);
-
-// Ajusta el carrusel si cambia el tamaño de la ventana
-window.addEventListener('resize', adjustCarousel);
+});
